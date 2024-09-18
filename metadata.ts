4184f86a4ab75df54dbe7639856170282590a5bb
Jeremiah Donley <108106416+WeiQiPro@users.noreply.github.com>
2024-09-18 00:36:05 -0400

    Delete metadata.ts

--- metadata.ts
+++
@@ -1,6 +0,0 @@
-const SCREEN_WIDTH: number = 480;
-const SCREEN_HEIGHT: number = 800;
-const SCREEN_TITLE: string = "Hello Raylib"
-const ENTRY_POINT: string = "./src/main.ts"
-
-export { SCREEN_HEIGHT, SCREEN_WIDTH, SCREEN_TITLE, ENTRY_POINT }
